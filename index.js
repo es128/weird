56d495bac796def1463858fe2186d44adcaf7bfd
Elan Shanker <elan.shanker@gmail.com>
2014-11-17 18:42:38 -0500

    Simpler AST walk

--- index.js
+++ index.js
@@ -18,82 +18,22 @@ var reserved = [
 var weirdIdentifiers = Object.create(null);
 function weirdAST(body) {
 	if (!body || !body.type) return;
-	switch (body.type) {
-    case 'Identifier':
-		if (reserved.indexOf(body.name) < 0) {
-			weirdIdentifiers[body.name] = body.name;
-			body.name = 'elan';
-		}
-		return;
-    case 'FunctionDeclaration':
-    case 'FunctionExpression':
-    	weirdAST(body.id);
-		//if (body.id) weirdIdentifiers[body.id.name] = body.id.name;
-		body.params.forEach(weirdAST);
-		weirdAST(body.body);
-    	return;
-    case 'VariableDeclaration':
-    	body.declarations.forEach(function(declaration) {
-			weirdAST(declaration.id);
-			weirdAST(declaration.init);
+	if (body.type === 'Identifier' && reserved.indexOf(body.name) < 0) {
+		weirdIdentifiers[body.name] = body.name;
+		body.name = 'elan';
+	} else {
+		Object.keys(body).forEach(function(key) {
+			if (key === 'property' || key === 'key') return;
+			var obj = body[key];
+			if (obj && obj.type) weirdAST(obj);
+			else if (Array.isArray(obj)) obj.forEach(weirdAST);
 		});
-    	return;
-    case 'SwitchStatement':
-		weirdAST(body.discriminant);
-		body.cases.forEach(function(kase) {
-			weirdAST(kase.test);
-			kase.consequent.forEach(weirdAST);
-		});
-    	return;
-    case 'TryStatement':
-		body.block.body.forEach(weirdAST);
-		body.handlers.forEach(function(handler) {
-			weirdAST(handler.param);
-			weirdAST(handler.body);
-		});
-		weirdAST(body.finalizer);
-    	return;
-    case 'ObjectExpression':
-		body.properties.map(function(prop){
-			return prop.value;
-		}).forEach(weirdAST);
-		return;
-    case 'CallExpression':
-    case 'NewExpression':
-    	weirdAST(body.callee);
-		body.arguments.forEach(weirdAST);
-    	return;
-    case 'UpdateExpression':
-    case 'ReturnStatement':
-    	return weirdAST(body.argument);
-    case 'IfStatement':
-    case 'ConditionalExpression':
-		weirdAST(body.test);
-		weirdAST(body.consequent);
-		weirdAST(body.alternate);
-		return;
-    case 'MemberExpression':
-    	weirdAST(body.object);
-    	if (body.computed) weirdAST(body.property);
-    	return;
-    case 'AssignmentExpression':
-    case 'BinaryExpression':
-    case 'LogicalExpression':
-    case 'ForInStatement':
-		weirdAST(body.left);
-		weirdAST(body.right);
-		if (body.body) weirdAST(body.body);
-    	return;
-    case 'UnaryExpression': return weirdAST(body.argument);
-    case 'ArrayExpression': return body.elements.forEach(weirdAST);
-    case 'BlockStatement': return body.body.forEach(weirdAST);
-    case 'ExpressionStatement': return weirdAST(body.expression);
-    }
+	}
 }
-
 
 ast.program.body.forEach(weirdAST);
 
 console.log(recast.print(ast).code);
-console.log(weirdIdentifiers);
+console.log(Object.keys(weirdIdentifiers).sort());
+console.log(Object.keys(weirdIdentifiers).length);
 console.log(process.hrtime(startTime))
