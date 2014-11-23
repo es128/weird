c8648da8b0f5af5d3791f9603cd83b2e15f51bb5
Elan Shanker <elan.shanker@gmail.com>
2014-11-22 21:59:33 -0500

    Reserve toString explicitly again for now
    sindresorhus/globals#3

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ var reserved = Object.create(null);
 		reserved[global] = 1;
 	});
 });
+reserved.toString = 1;
 
 var weirdIdentifiers = Object.create(null);
 var weirdIdentifiersInv = Object.create(null);
